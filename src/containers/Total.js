6267d424cb236778446a233416da73567d40cd35
Johnson Kwizera <johnsonkwizera@Johnsons-MacBook-Pro.local>
2020-07-16 03:41:40 -0500

    week 5

---
+++ src/containers/Total.js
@@ -0,0 +1,9 @@
+  
+import { connect } from 'react-redux'
+import Total from '../components/Total'
+const mapStateToProps = (state) => {
+    return {
+        cars: state.cars
+    }
+}
+export default connect(mapStateToProps)(Total)
